859f562b5ad95b36734a43f55b36d0b0e153ead0
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-08-01 14:08:05 +0530

    Schema updated

---
+++ src/admin/index.ts
@@ -0,0 +1,25 @@
+import { Module } from "../../libs/utils/types/module"
+import express from "express";
+
+const router = express.Router();
+
+const BASE_ROUTE = "/admin";
+
+// GET
+router.get("/", async () => { });
+
+// POST
+router.post("/", async () => { });
+
+// PUT
+router.put("/", async () => { });
+
+// DELETE
+router.delete("/", async () => { });
+
+const MODULE: Module = {
+    router,
+    BASE_ROUTE
+};
+
+export default MODULE;
