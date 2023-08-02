9dd10d0d11a7b925a68bdf4f4252f33e642999c7
Naveen M P <120002392+navi-prem@users.noreply.github.com>
2023-08-02 14:20:29 +0530

    Timline CRUD API (#3)
    * pulling latest commits
    
    * timeline CRUD API done.
    
    * Bug fixes

--- src/timeline/index.ts
+++ src/timeline/index.ts
@@ -1,24 +1,107 @@
 import { Module } from "../../libs/utils/types/module"
 import express from "express";
+import { prisma } from "../../libs/utils/prisma";
+import { value, major, timelines } from "./types";
 
 const router = express.Router();
 
 const BASE_ROUTE = "/timeline";
 
 // GET
-router.get("/", async () => {
+router.get("/", async (_, res) => {
+    const data: Array<timelines> = await prisma.year.findMany({
+        include: {
+            timelines: {
+                orderBy: {
+                    date: 'asc',
+                },
+            },
+        },
+    })
+    let response: Array<major> = []
+    data.forEach((element: timelines) => {
+        let dict: major = { events: [], year: 0, }
+        dict['year'] = element.id
+        element.timelines.forEach((ele: any) => {
+            let obj: value = {
+                date: ele.date || new Date(0),
+                title: ele.title || '',
+                content: ele.content || [],
+                id: ele.id || 0
+            }
+            dict.events.push(obj)
+        })
+        response.push(dict)
+    });
+    res.json(response).status(301)
+
 });
 
 // POST
-router.post("/", async () => {
+router.post("/", async (req, res) => {
+    const { title, content }: { title: string, content: Array<string> } = req.body
+    let { date }: { date: Date } = req.body
+    date = date ? new Date(date) : new Date(Date.now())
+    let { year_id }: { year_id: number } = req.body
+    year_id = year_id ?? (new Date(date)).getFullYear()
+
+    const check = await prisma.year.findUnique({
+        where: {
+            id: year_id,
+        },
+    })
+
+    let data
+
+    if (check) data = await prisma.timeline.create({
+        data: {
+            date,
+            title,
+            content,
+            year_id,
+        }
+    })
+    else {
+        await prisma.year.create({
+            data: {
+                id: year_id,
+                timelines: {
+                    create: {
+                        date,
+                        title,
+                        content,
+                    }
+                }
+            }
+        })
+    }
+    res.json({ data })
 });
 
 // PUT
-router.put("/", async () => {
+router.put("/", async (req, res) => {
+    const { id, title, content } = req.body
+    const data = await prisma.timeline.update({
+        where: {
+            id,
+        },
+        data: {
+            title,
+            content,
+        }
+    })
+    res.json({ data }).status(200)
 });
 
 // DELETE
-router.delete("/", async () => {
+router.delete("/", async (req, res) => {
+    const { id } = req.body
+    const data = await prisma.timeline.delete({
+        where: {
+            id
+        },
+    })
+    res.json({ data, }).status(200)
 });
 
 const MODULE: Module = {
--- src/timeline/types.d.ts
+++ src/timeline/types.d.ts
@@ -1,1 +1,22 @@
-// Types for the timeline module
+export type value = {
+    date: Date,
+    title: string,
+    content: Array<string>,
+    id: number,
+}
+
+export type major = {
+    year: number,
+    events: Array<value>,
+}
+
+export type timelines = {
+    id: number,
+    timelines: Array<{
+        id: number,
+        title: string,
+        content: string[],
+        date: Date,
+        year_id: number
+    }>
+}
