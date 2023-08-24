ba762c902e92da2837a470a08b19aa52da447c2b
Rahul M. Navneeth <50259284+RahulNavneeth@users.noreply.github.com>
2023-08-24 11:04:40 +0530

    (HOST) 🍙 (#11)

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,13 @@ import Timeline from "./timeline"
 import Admin from "./admin"
 import me from "./me"
 import { AuthMiddleware } from "./middleware/auth"
+import { client_base_url } from "../libs/constants"
 
 const PORT: number = 6969;
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }), bodyParser.json(), cookieParser());
+app.use(cors({ origin: client_base_url, credentials: true }), bodyParser.json(), cookieParser());
 
 app.use(Auth.BASE_ROUTE, Auth.router);
 app.use(Timeline.BASE_ROUTE, Timeline.router);
