ed41e861a2f992bd51238d8f929f5dbc74ce0601
Rahul M. Navneeth <50259284+RahulNavneeth@users.noreply.github.com>
2023-08-24 09:49:05 +0530

    Env (#10)
    * OOF (AUTH) 🍙
    
    * (BUILD) 🍙
    
    * (VERCEL T1) 🍙
    
    * (VERCEL T2) 🍙
    
    * (VERCEL T3) 🍙
    
    * (ENV) 🍙
    
    * (DIST) 🍙

--- libs/constants.ts
+++ libs/constants.ts
@@ -1,9 +1,10 @@
-export const production: boolean = false;
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const production: boolean = process.env.PRODUCTION === 'true';
 export const base_url = !production ? 'http://localhost:6969' : 'https://api.bitspace.org.in';
+export const client_base_url = !production ? 'http://localhost:3000' : 'https://bitspace.org.in';
 
-export const client_production: boolean = true;
-export const client_base_url = !client_production ? 'http://localhost:3000' : 'https://bitspace.org.in';
-
-export const client_id = "e49d77123e22ded7e2e0"
-export const client_secret = "ccb7b74a08150afc8a0a834cef39fb76b9e6f441"
+export const client_id = process.env.GITHUB_CLIENT_ID;
+export const client_secret = process.env.GITHUB_CLIENT_SECERT
 export const redirect_uri = `${client_base_url}/github/auth/callback`
