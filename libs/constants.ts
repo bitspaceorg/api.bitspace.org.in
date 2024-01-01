556cf7ed8843d70198ef8408c5ab4adba3c86429
Naveen M P <120002392+navi-prem@users.noreply.github.com>
2024-01-01 14:03:16 +0530

    Debugging #1 (#22)

--- libs/constants.ts
+++ libs/constants.ts
@@ -6,5 +6,5 @@ export const base_url = !production ? 'http://localhost:6969' : 'https://api.bit
 export const client_base_url = !production ? 'http://localhost:3000' : 'https://www.bitspace.org.in';
 
 export const client_id = process.env.GITHUB_CLIENT_ID;
-export const client_secret = process.env.GITHUB_CLIENT_SECERT
+export const client_secret = process.env.GITHUB_CLIENT_SECRET
 export const redirect_uri = `${client_base_url}/github/auth/callback`
